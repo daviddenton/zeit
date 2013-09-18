910dbba586b259525197cc243aa84f99198f53b8
David Denton <denton.david@gmail.com>
2013-09-18 10:43:53 +0100

    added simple clock, moment clock, and stub moment clock

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var _ = require('lodash');
+
+_.extend(module.exports, require('./lib/clock.js'));
---
+++ test/clockTest.js
@@ -0,0 +1,69 @@
+'use strict'
+var zeit = require('../'),
+    moment = require('moment'),
+    assert = require('chai').assert;
+
+describe('a date based clock', function () {
+    it('now()', function () {
+        assert.equal(new zeit.DateClock().now().toString(), new Date().toString());
+    });
+});
+
+describe('a moment based clock', function () {
+    it('now()', function () {
+        assert.equal(new zeit.MomentClock().now().toString(), moment().toString());
+    });
+});
+
+assert.momentEql = function(expected, actual) {
+    assert.equal(expected.toString(), actual.toString());
+}
+
+describe('a stub moment clock', function () {
+    var DEFAULT_TIME = moment(0);
+    var DEFAULT_TICK = moment.duration(1, 'second');
+    var customTick = moment.duration(2, 'second');
+
+    var clock = new zeit.StubMomentClock();
+
+    it('now() uses default time', function () {
+        assert.momentEql(clock.now(), DEFAULT_TIME);
+    });
+
+    it('now() using custom time', function () {
+        var nonDefaultTime = moment(1234);
+        assert.momentEql(new zeit.StubMomentClock(nonDefaultTime).now(), nonDefaultTime);
+    });
+
+    it('now() can modify time', function () {
+        var nonDefaultTime = moment(1234);
+        assert.momentEql(new zeit.StubMomentClock().now(nonDefaultTime), nonDefaultTime);
+    });
+
+    it('tick() uses default ticksize', function () {
+        var clock = new zeit.StubMomentClock();
+        assert.momentEql(clock.tick(), moment(DEFAULT_TIME).add(DEFAULT_TICK));
+        assert.momentEql(clock.now(), moment(DEFAULT_TIME).add(DEFAULT_TICK));
+    });
+
+    it('tick() set to ticksize set on construction', function () {
+        var clock = new zeit.StubMomentClock(DEFAULT_TIME, customTick);
+        assert.momentEql(clock.tick(), moment(DEFAULT_TIME).add(customTick));
+        assert.momentEql(clock.now(), moment(DEFAULT_TIME).add(customTick));
+    });
+
+    it('tick() by custom amount', function () {
+        var clock = new zeit.StubMomentClock();
+        assert.momentEql(clock.tick(customTick), moment(DEFAULT_TIME).add(customTick));
+        assert.momentEql(clock.now(), moment(DEFAULT_TIME).add(customTick));
+    });
+
+    it('tickSize() modifies tick size', function () {
+        var nonStandardTick = moment.duration(1, 'second');
+        var clock = new zeit.StubMomentClock();
+        clock.tick();
+        clock.tickSize(nonStandardTick);
+        clock.tick();
+        assert.momentEql(clock.now(), moment(DEFAULT_TIME).add(DEFAULT_TICK).add(nonStandardTick));
+    });
+});
