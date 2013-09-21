40a84116c88b4fb9ebdd5847a2f28ed3406b0448
David Denton <denton.david@gmail.com>
2013-09-21 19:09:49 +0100

    publich script

--- publish.sh
+++ publish.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-newVersion=$1
+newVersion=\"$1\"
 oldVersion=`./tools/jq .version package.json`
 name=`./tools/jq .name package.json`
 echo Publishing: $name version \"$newVersion\" \(was $oldVersion\)
 
-./tools/jq .version=$newVersion package.json > package.json
+./tools/jq .version=$newVersion package.json > $newVersion.tmp
+
+mv $newVersion.tmp package.json
 
 git add package.json
 git commit -m "upped version number to $newVersion"
