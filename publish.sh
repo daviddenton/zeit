3a5ae7465d458558c3074ec88eaba20aed565805
David Denton <denton.david@gmail.com>
2013-09-21 19:19:18 +0100

    publich script. refactor

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-newVersion=\"$1\"
+newVersion=$1
 oldVersion=`./tools/jq .version package.json`
 name=`./tools/jq .name package.json`
-echo Publishing: $name version \"$newVersion\" \(was $oldVersion\)
+echo Publishing: $name version $newVersion \(was $oldVersion\)
 
-./tools/jq .version=$newVersion package.json > $newVersion.tmp
+./tools/jq .version=\"$newVersion\" package.json > $newVersion.tmp
 
 mv $newVersion.tmp package.json
 
